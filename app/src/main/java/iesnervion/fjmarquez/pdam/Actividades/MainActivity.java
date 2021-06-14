ec84ebfcbf157af6dbc381aaa5ebcd24a9355c18
Francisco José Márquez Pulido <43822565+fjmarquez@users.noreply.github.com>
2021-06-14 12:38:18 +0200

    Limpiza en MainActivity y Útiles, comentarios en Útiles

--- app/src/main/java/iesnervion/fjmarquez/pdam/Actividades/MainActivity.java
+++ app/src/main/java/iesnervion/fjmarquez/pdam/Actividades/MainActivity.java
@@ -98,8 +98,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Utiles.pruebaFirebase();
-
         inicializar();
 
         fragmentInicial();
